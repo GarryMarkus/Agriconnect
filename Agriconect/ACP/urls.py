d955f6b09e63e7124c8ddabfc5a33c7073a3cf42
Prakkhar <prakkhar.n03@gmail.com>
2025-01-13 13:50:29 +0530

    Connecting backed and frontend

--- Agriconect/ACP/urls.py
+++ Agriconect/ACP/urls.py
@@ -3,6 +3,7 @@ from ACP.views import *
 
 urlpatterns = [
     path('',start_template, name='home'),
-    path("login/",login,name="login")
+    path("login/",login,name="login"),
+    path("register/",register,name="register")
     
     ]
--- Agriconect/ACP/views.py
+++ Agriconect/ACP/views.py
@@ -5,3 +5,5 @@ def start_template(request):
     return render(request, 'index.html')
 def login(request):
     return render(request, 'login.html')
+def register(request):
+    return render(request, 'register.html')
