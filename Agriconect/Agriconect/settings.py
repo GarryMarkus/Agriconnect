aafa2478cabf57025dff744e2a33bd3f71e13d58
Prakkhar <prakkhar.n03@gmail.com>
2025-02-13 16:12:41 +0530

    Change database

--- Agriconect/Agriconect/settings.py
+++ Agriconect/Agriconect/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'Agriconect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agriconnect_db',
+        'USER': 'agriconnect_user', 
+        'PASSWORD': 'admin@123',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
