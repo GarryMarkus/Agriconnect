83d91b6d1365cb1cea98266bafb5b6312a94b80c
Prakhar Mehrotra <154071238+prakkhar03@users.noreply.github.com>
2025-04-07 17:59:09 +0530

    Update settings.py

--- Agriconect/Agriconect/settings.py
+++ Agriconect/Agriconect/settings.py
@@ -153,10 +153,11 @@ JAZZMIN_SETTINGS = {
         {"name": "Order Assignment", "url": "/admin-dashboard/", "icon": "fas fa-tachometer-alt"},
     ],
 }
+password=os.getenv("PASSWORD")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER ='rocksbabaji41217@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
+EMAIL_HOST_PASSWORD =password.replace('\xa0', ' ')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
