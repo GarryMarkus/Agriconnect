74fff9dd0197c21175bb5a138a6f1c5f0741df1d
Prakhar Mehrotra <154071238+prakkhar03@users.noreply.github.com>
2025-04-07 16:25:25 +0530

    Update settings.py

--- Agriconect/Agriconect/settings.py
+++ Agriconect/Agriconect/settings.py
@@ -158,5 +158,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER ='rocksbabaji41217@gmail.com'
-EMAIL_HOST_PASSWORD = 'jopa irsc vynx bblo'
+EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
